9d0ffd30fa7beadb4d9624094715ae71bafc6e9a
Tim Brust <github@timbrust.de>
2020-01-06 22:17:25 +0100

    WIP

--- lib/bearssl-esp8266/src/t_bearssl_tasmota_config.h
+++ lib/bearssl-esp8266/src/t_bearssl_tasmota_config.h
@@ -16,19 +16,19 @@
 #endif
 
 #ifndef BR_SLOW_MUL15
-#define BR_SLOW_MUL15 1         // shrinks EC code by 8.5k
+#define BR_SLOW_MUL15 1 // shrinks EC code by 8.5k
 #endif
 
 #ifndef BR_MAX_RSA_SIZE
 #ifdef USE_4K_RSA
-#define BR_MAX_RSA_SIZE 4096    // max 4096 bits RSA keys
+#define BR_MAX_RSA_SIZE 4096 // max 4096 bits RSA keys
 #else
-#define BR_MAX_RSA_SIZE 2048    // max 2048 bits RSA keys
+#define BR_MAX_RSA_SIZE 4096 // max 2048 bits RSA keys
 #endif
 #endif
 
 #ifndef BR_MAX_EC_SIZE
-#define BR_MAX_EC_SIZE 256      // max 256 bits EC keys
+#define BR_MAX_EC_SIZE 512 // max 256 bits EC keys
 #endif
 
 #endif
